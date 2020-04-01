4c40ab5d243e599555afac165b414edd2a3d56bc
Syamlal CM <syamjayaraj@gmail.com>
2020-04-01 17:35:54 +0530

    added style

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React, { Component } from 'react';
-import Button from './components/Button';
-
+import React, { Component } from "react";
+import Button from "./components/Button";
+import "./assets/css/style.css";
 export default class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       count: 0
-    }
+    };
   }
 
   incrementCount = () => {
     this.setState({
       count: this.state.count + 1
-    })
-  }
-  
+    });
+  };
+
   decrementCount = () => {
     this.setState({
       count: this.state.count - 1
-    })
-  }
+    });
+  };
 
   render() {
-    let { count } = this.state
+    let { count } = this.state;
     return (
-      <div className="App">
-        <h2>Count: { count }</h2>
-        <Button 
-          title = { "+" }
-          task = { () => this.incrementCount() }
-        />
-        <Button
-          title = { "-" }
-          task = { () => this.decrementCount() }
-        />
+      <div className="app">
+        <div>
+          <div class="count">
+            <h3>Count:</h3>
+            <h1>{count}</h1>
+          </div>
+          <div class="buttons">
+            <Button title={"-"} action={this.decrementCount} />
+            <Button title={"+"} action={this.incrementCount} />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Button extends Component {
+  render() {
+    return <button onClick={this.props.action}>{this.props.title}</button>;
+  }
+}
